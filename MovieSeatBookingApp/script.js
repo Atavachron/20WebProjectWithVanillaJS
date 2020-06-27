dd4dcbb047b01a390cc1e9935c7f7ed903a731da
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-06-27 18:04:07 +0400

    Add items to local storage

--- MovieSeatBookingApp/script.js
+++ MovieSeatBookingApp/script.js
@@ -5,24 +5,39 @@ const count = document.querySelector('.count');
 const total = document.querySelector('.total');
 let ticketPrice = Number(movie.value);
 
+//Update the total seats and update local storage
 function updateTotal() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
   let totalSelectedSeats = selectedSeats.length;
   count.textContent = totalSelectedSeats;
   total.textContent = totalSelectedSeats * ticketPrice;
+
+  let indicesArray = [...selectedSeats].map(currentSeat =>
+    [...seats].indexOf(currentSeat)
+  );
+
+  localStorage.setItem('selectedSeats', JSON.stringify(indicesArray));
+}
+
+//Save the selected movie in the drop down menu and the price
+function updateMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
 container.addEventListener('click', e => {
   if (
     e.target.classList.contains('seat') &&
     !e.target.classList.contains('occupied')
-  )
+  ) {
     e.target.classList.toggle('selected');
-  updateTotal();
+    updateTotal();
+  }
 });
 
 movie.addEventListener('change', e => {
   ticketPrice = Number(e.target.value);
+  updateMovieData(e.target.selectedIndex, e.target.value);
   updateTotal();
 });
