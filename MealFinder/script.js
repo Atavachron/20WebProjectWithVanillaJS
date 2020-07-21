f370c1f5062b7f6e518719c467cf6829a0915457
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-07-21 23:48:19 +0400

    Add DOM variables

---
+++ MealFinder/script.js
@@ -0,0 +1,6 @@
+const $search = document.getElementById('search');
+const $submit = doument.getElementById('submit');
+const $random = document.getElementById('random');
+const $meals = document.getElementById('meals');
+const $resultHeading = document.getElementById('result-heading');
+const $singleMeal = document.getElementById('single-meal');
