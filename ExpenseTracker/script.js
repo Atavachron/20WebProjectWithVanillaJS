1fac4028f8df80ad6e7c41f4d7594490c8583a32
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-10-03 00:21:58 +0400

    Show transactions items in the history

---
+++ ExpenseTracker/script.js
@@ -0,0 +1,46 @@
+//DOM variables
+
+const balance = document.querySelector('#balance');
+const moneyPlus = document.querySelector('#money-plus');
+const moneyMinus = document.querySelector('#money-minus');
+const list = document.querySelector('#list');
+const form = document.querySelector('#form');
+const text = document.querySelector('#text');
+const amount = document.querySelector('#amount');
+
+const testTransactions = [
+  { id: 1, text: 'Groceries', amount: -50 },
+  { id: 2, text: 'Salary', amount: 2000 },
+  { id: 3, text: 'Book', amount: -60 },
+  { id: 4, text: 'Lottery', amount: 1000 },
+];
+
+let transactions = testTransactions;
+
+function addTransactionToDOM(transaction) {
+  //Create a new list element
+  let li = document.createElement('li');
+  //Add the respective class, depending on the amount
+  if (transaction.amount < 0) {
+    li.classList.add('minus');
+  } else {
+    li.classList.add('plus');
+  }
+  //Set the inner html of the new list item to the desired format
+
+  li.innerHTML = `
+  ${transaction.text}<span>${transaction.amount}</span><button class="delete-btn">x</button>`;
+
+  //Append the list item to the unordered list
+  list.appendChild(li);
+}
+
+function init() {
+  //Clear the displayed history when the init function runs
+  list.innerHTML = '';
+
+  //Display the transactions in the history
+  transactions.forEach(addTransactionToDOM);
+}
+
+init();
