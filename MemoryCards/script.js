c37be0ebe82f0ee1b6dc13be66fdaea1920bed63
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-11-11 23:58:43 +0400

    Add prev and next button functionality

--- MemoryCards/script.js
+++ MemoryCards/script.js
@@ -80,3 +80,31 @@ function updateCurrentEl() {
 }
 
 createCards();
+
+$nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+
+  currentActiveCard = currentActiveCard + 1;
+
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cards.length - 1;
+  }
+
+  cardsEl[currentActiveCard].clasName = 'card active';
+
+  updateCurrentEl();
+});
+
+$prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+
+  currentActiveCard = currentActiveCard - 1;
+
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+
+  cardsEl[currentActiveCard].clasName = 'card active';
+
+  updateCurrentEl();
+});
