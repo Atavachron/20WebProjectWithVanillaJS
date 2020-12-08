e7f7438799cb3450b03aa72e761a6d8337c3a272
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-12-08 23:08:53 +0400

    Draw ball on the canvas

--- BreakoutGame/script.js
+++ BreakoutGame/script.js
@@ -2,6 +2,29 @@ const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 
+//Create canvas context
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+//Create an object with ball properties
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
+//Draw the ball on the canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
 //Event Handlers for the rules and close buttons
 rulesBtn.addEventListener('click', () => {
   rules.classList.add('show');
@@ -10,3 +33,5 @@ rulesBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   rules.classList.remove('show');
 });
+
+drawBall();
