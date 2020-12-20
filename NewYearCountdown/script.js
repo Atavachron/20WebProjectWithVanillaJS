ba8c7cca081a9bde5bb67872ae2401fa16ea68df
Atavachron <37329317+Atavachron@users.noreply.github.com>
2020-12-20 23:52:29 +0400

    Add spinner when loading

--- NewYearCountdown/script.js
+++ NewYearCountdown/script.js
@@ -3,10 +3,14 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+year.innerText = currentYear + 1;
 
 function updateCountdown() {
   const now = new Date();
@@ -29,5 +33,11 @@ function updateCountdown() {
   minutes.innerHTML = m < 10 ? '0' + m : m;
   seconds.innerHTML = s < 10 ? '0' + s : s;
 }
+
+// Show the spinner before the countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
 
 setInterval(updateCountdown, 1000);
